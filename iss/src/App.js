4f07131b32601d96f63f510da7ff0766b37a8ae2
Teigan H <teigan.hockman@gmail.com>
2019-03-25 11:57:54 -0400

    Create Home.js and About.js components.

--- iss/src/App.js
+++ iss/src/App.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route,Link } from "react-router-dom";
+import About from './components/About.js'
+import Home from './components/Home.js'
+import './App.css'
+import {css} from 'https://unpkg.com/leaflet@1.4.0/dist/leaflet.css'
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(props){
+    super(props)
+    this.state={}
   }
+
+  getISS(){
+    fetch('http://api.open-notify.org/iss-now.json')
+    .then(response=>response.json())
+    .then(json=>{
+      this.setState({
+        latitude: json.iss_position.latitude,
+        longitude: json.iss_position.longitude,
+      })
+    }) 
+  }
+
+  render() {
+  
+   return (
+      
+      <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        
+      </nav>
+      <main>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </main>
+        <button onClick={()=>this.getISS()}>Find the ISS</button>
+        <div>
+          <h2>{this.state.latitude}</h2>
+          <h2>{this.state.longitude}</h2>
+        </div>
+      </div>
+    )
+    }
 }
 
 export default App;
